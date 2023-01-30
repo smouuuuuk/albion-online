00867fe0b73abe27d609aaa1398af7c47d2400e2
Rotchamar <66692778+Rotchamar@users.noreply.github.com>
2023-01-30 01:29:59 +0100

    Update pyGoogleCloud.py

--- bloque1/pyGoogleCloud.py
+++ bloque1/pyGoogleCloud.py
@@ -6,6 +6,8 @@ import json
 import time
 from subprocess import call
 
+call(["export GROUP_NUMBER=38"], shell=True)
+
 call(["echo 'y' | sudo apt-get update"], shell=True)
 call(["echo 'y' | sudo apt install python3-pip"], shell=True)
 call(["git clone https://github.com/CDPS-ETSIT/practica_creativa2.git"], shell=True) #Descargamos los archivos almacenados en el directorio de GitHub de la práctica
@@ -19,9 +21,7 @@ call(["echo 'y' | sudo apt-get update"], shell=True)
 fin = open("practica_creativa2/bookinfo/src/productpage/productpage_monolith.py", 'r') # Fichero de entrada necesario para ejecutar la aplicación
 fout = open("practica_creativa2/bookinfo/src/productpage/productpage_monolith2.py", 'w') # Fichero de salida
 for line in fin:
-  if "reviewsHostname = "+r'"'+"reviews"+r'"'+" if (os.environ.get("+r'"'+"REVIEWS_HOSTNAME"+r'"'+") is None) else os.environ.get("+r'"'+"REVIEWS_HOSTNAME"+r'"'+")" in line :
-   fout.write(line+ "\nos.environ['GROUP_NUMBER']="+r'"'+"Grupo 38"+r'"'+" \n") 
-  elif "'title': 'The Comedy of Errors'," in line :
+  if "'title': 'The Comedy of Errors'," in line :
     fout.write("'title': 'The Comedy of Errors ' +os.environ['GROUP_NUMBER'],")
   elif "'author': book['authors'][0]," in line :
     fout.write(line + "        'grupo' : os.environ['GROUP_NUMBER'],\n")
