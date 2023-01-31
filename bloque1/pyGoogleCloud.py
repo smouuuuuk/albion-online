2e19909c4d0aeca803f6f43df96701d61cbcb1be
Rotchamar <66692778+Rotchamar@users.noreply.github.com>
2023-01-31 12:27:45 +0100

    Update pyGoogleCloud.py

--- bloque1/pyGoogleCloud.py
+++ bloque1/pyGoogleCloud.py
@@ -1,16 +1,21 @@
-#Google Cloud (FALTA dirección IP Pública)
+#Google Cloud (Configurar primero el firewall)
 
 #Importamos las librerías necesarias:
-from subprocess import call
+import subprocess
 import os
+
+
+def call(comando):
+    subprocess.call([comando], shell=True)
+
 
 os.environ["GROUP_NUMBER"] = "38"
 
-call(["sudo apt-get update -y"], shell=True)
-call(["sudo apt install python3-pip -y"], shell=True)
-call(["git clone https://github.com/CDPS-ETSIT/practica_creativa2.git"], shell=True) #Descargamos los archivos almacenados en el directorio de GitHub de la práctica
-call(["pip3 install urllib3"], shell=True)
-call(["pip3 install -r practica_creativa2/bookinfo/src/productpage/requirements.txt"], shell=True) #Instalamos las dependencias especificadas en el fichero requirements.txt usando pip(3)
+call("sudo apt-get update -y")
+call("sudo apt install python3-pip -y")
+call("git clone https://github.com/CDPS-ETSIT/practica_creativa2.git") #Descargamos los archivos almacenados en el directorio de GitHub de la práctica
+call("pip3 install urllib3")
+call("pip3 install -r practica_creativa2/bookinfo/src/productpage/requirements.txt") #Instalamos las dependencias especificadas en el fichero requirements.txt usando pip(3)
 
 fin = open("practica_creativa2/bookinfo/src/productpage/productpage_monolith.py", 'r') # Fichero de entrada necesario para ejecutar la aplicación
 fout = open("practica_creativa2/bookinfo/src/productpage/productpage_monolith2.py", 'w') # Fichero de salida
@@ -23,10 +28,10 @@ for line in fin:
    fout.write(line)
 fin.close()
 fout.close()
-               
-call(["sudo","cp","practica_creativa2/bookinfo/src/productpage/productpage_monolith2.py","practica_creativa2/bookinfo/src/productpage/productpage_monolith.py"])
-call(["sudo","rm","practica_creativa2/bookinfo/src/productpage/productpage_monolith2.py"])
-                           
+
+call("sudo cp practica_creativa2/bookinfo/src/productpage/productpage_monolith2.py practica_creativa2/bookinfo/src/productpage/productpage_monolith.py")
+call("sudo rm practica_creativa2/bookinfo/src/productpage/productpage_monolith2.py")
+
 fin = open("practica_creativa2/bookinfo/src/productpage/templates/productpage.html", 'r') 
 fout = open("practica_creativa2/bookinfo/src/productpage/templates/productpage2.html", 'w') 
 for line in fin:
@@ -37,8 +42,8 @@ for line in fin:
 fin.close()
 fout.close()
 
-call(["sudo","cp","practica_creativa2/bookinfo/src/productpage/templates/productpage2.html","practica_creativa2/bookinfo/src/productpage/templates/productpage.html"])
-call(["sudo","rm","practica_creativa2/bookinfo/src/productpage/templates/productpage2.html"])
+call("sudo cp practica_creativa2/bookinfo/src/productpage/templates/productpage2.html practica_creativa2/bookinfo/src/productpage/templates/productpage.html")
+call("sudo rm practica_creativa2/bookinfo/src/productpage/templates/productpage2.html")
 
-call(["python3 practica_creativa2/bookinfo/src/productpage/productpage_monolith.py 9080"], shell=True)
+call("python3 practica_creativa2/bookinfo/src/productpage/productpage_monolith.py 9080")
 #9080 es el puerto en el que queremos que la aplicación reciba las peticiones
