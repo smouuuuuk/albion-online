f8b95cc97929587bf9365aed7a9edc15876b30b8
Rotchamar <66692778+Rotchamar@users.noreply.github.com>
2023-01-31 13:19:54 +0100

    Update main.py

--- helm-charts/main.py
+++ helm-charts/main.py
@@ -16,7 +16,7 @@ call("minikube profile pc2")
 call("helm install pc2 .")
 
 # Esperamos hasta que todos los pods se hayan iniciado
-while (str(subprocess.check_output(["kubectl get pods"])).count("Running") < 9):
+while (str(subprocess.check_output(["kubectl get pods"], shell=True)).count("Running") < 9):
     time.sleep(1)
 
 # Exponemos el puerto 9080 de la product-page
