6e2ff38d4c6342e19ae99a09816517507063746d
Rotchamar <66692778+Rotchamar@users.noreply.github.com>
2023-01-31 12:50:54 +0100

    Update main.py

--- bloque2/main.py
+++ bloque2/main.py
@@ -3,7 +3,7 @@ import subprocess
 def call(comando):
     subprocess.call([comando], shell=True)
 
-call (["git clone https://github.com/smouuuuuk/practica_creativa2.git"])
-call (["docker build -t g38/product-page:mono ."])
-call (["rm -rf ./practica_creativa2"])
-call (["docker run --rm -it --name g38-product-page -p 9080:9080 g38/product-page:mono"])
+call ("git clone https://github.com/smouuuuuk/practica_creativa2.git")
+call ("docker build -t g38/product-page:mono .")
+call ("rm -rf ./practica_creativa2")
+call ("docker run --rm -it --name g38-product-page -p 9080:9080 g38/product-page:mono")
