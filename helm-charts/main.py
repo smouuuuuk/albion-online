a170ff93739f9ba4dfca334f5a99ed8b888ec8db
smouuuuuk <alexander.cabellop@gmail.com>
2023-01-31 12:56:25 +0100

    Update main.py

--- helm-charts/main.py
+++ helm-charts/main.py
@@ -7,20 +7,20 @@ def call(comando):
 #Necesario tener helm instalado
 
 # Necesitamos primero iniciar un cluster de Kubernetes
-call(["minikube start --driver=docker -p pc2 --nodes 5"])
+call("minikube start --driver=docker -p pc2 --nodes 5")
 
 # Cambiamos al perfil que acabamos de crear
-call(["minikube profile pc2"])
+call("minikube profile pc2")
 
 # Desplegamos helm sobre el cluster creado
-call(["helm install pc2 ."])
+call("helm install pc2 .")
 
 # Esperamos hasta que todos los pods se hayan iniciado
 while (str(subprocess.check_output(["kubectl get pods"])).count("Running") < 9):
     time.sleep(1)
 
 # Exponemos el puerto 9080 de la product-page
-call(["kubectl expose deployment product-page-latest --type=NodePort --port=9080"])
+call("kubectl expose deployment product-page-latest --type=NodePort --port=9080")
 
 # Abrimos la web de product-page
-call(["minikube service product-page-latest"])
+call("minikube service product-page-latest")
