17b1b56c013c4d965574e81460bfcce31336fd4f
Rotchamar <66692778+Rotchamar@users.noreply.github.com>
2023-01-31 12:52:42 +0100

    Update main.py

--- bloque4/main.py
+++ bloque4/main.py
@@ -6,17 +6,17 @@ def call(comando):
 
 
 # Iniciamos un clusetr de Kubernetes con 5 nodos
-call(["minikube start --driver=docker -p pc2 --nodes 5"])
+call("minikube start --driver=docker -p pc2 --nodes 5")
 
 # Cambiamos al perfil que acabamos de crear
-call(["minikube profile pc2"])
+call("minikube profile pc2")
 
 # Desplegamos los servicios y deployments
-call(["kubectl apply -f ."])
+call("kubectl apply -f .")
 
 # Esperamos hasta que todos los pods se hayan iniciado
 while (str(subprocess.check_output(["kubectl get pods"])).count("Running") < 9):
     time.sleep(1)
 
 # Abrimos la web de product-page
-call(["minikube service product-page"])
+call("minikube service product-page")
